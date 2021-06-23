127be51d697a28858e1d9206d6ed7fae6ec6dadd
Izumo1982 <84167062+Izumo1982@users.noreply.github.com>
2021-06-23 19:52:07 +0900

    Add files via upload

---
+++ FizzBuzz.java
@@ -0,0 +1,17 @@
+﻿public class FizzBuzz{
+
+	static String fizzbuzz(int i) {
+		String str = String.valueOf(i);
+		str = i % 3 == 0 ? "Fizz" : str;
+		str = i % 5 == 0 ? "Buzz" : str;
+		str = i % 5 == 0 && i % 3 == 0 ? "Fizz Buzz" : str;
+		return str;
+	}
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 100; i++){
+			System.out.println(fizzbuzz(i));
+		}
+	}
+
+}
